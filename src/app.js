598f93f0abd172824e7c8d64a46354a4c4ead325
Heider.F <heiderfandino@gmail.com>
2025-01-27 10:09:39 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,43 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let who = [
+  "El perro",
+  "Mi vecino",
+  "Mi caballo",
+  "Mi ex",
+  "Mi suegra",
+  "El extraterrestre",
+  "Mi cerebro"
+];
+let what = [
+  "salio volando",
+  " se bugeo",
+  " exploto",
+  "fallo",
+  " se durmio",
+  "se murio",
+  "se auto-destruyo"
+];
+let when = [
+  "cuando me cai.",
+  "cuando jugaba.",
+  "cuando estaba de fiesta.",
+  "cuando lo compre.",
+  "cuando lo vi.",
+  "cuando lo use.",
+  "cuando lo toque."
+];
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+function generarExcusa() {
+  let whoIndex = Math.floor(Math.random() * who.length);
+  let whatIndex = Math.floor(Math.random() * what.length);
+  let whenIndex = Math.floor(Math.random() * when.length);
+  return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
+}
+function mostrarExcusa() {
+  let excusa = generarExcusa();
+  document.getElementById("excusa").innerText = excusa;
+}
+
+window.onload = mostrarExcusa;
+
+console.log("Hello Rigo from the console!");
